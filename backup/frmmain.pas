0d9d129763e446e48467dfc224dd2d9f0dd2b7d6
Hinged-Cocoa <162573473+Hinged-Cocoa@users.noreply.github.com>
2024-04-02 20:43:50 +0200

    This was the project due 3rd April

---
+++ backup/frmmain.pas
@@ -0,0 +1,49 @@
+unit frmMain;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    btnGenerate: TButton;
+    edtBase: TEdit;
+    Label1: TLabel;
+    Label2: TLabel;
+    lblResult: TLabel;
+    procedure btnGenerateClick(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.btnGenerateClick(Sender: TObject);
+var
+  index: integer;
+  base: integer;
+begin
+ base := strToInt (edtBase.caption);
+ lblResult.caption := 'The division table of ' + intToStr(base) + ' divided by 1 - 20 is' + #10#13 + #10#13;
+  for index := 1 to 20 do
+ begin
+   lblResult.caption := lblResult.caption + intToStr (base) + ' / ' + intToStr (index) + ' = ' + intToStr ((base / index)) + #10#13;
+ end;
+
+ end;
+end.
